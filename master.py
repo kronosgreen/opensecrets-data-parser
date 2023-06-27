c9c469c23994249cc85dcdcd35f5ba0bac58b0d1
Christopher Medrano <703137326@uniparks.com>
2023-06-26 23:37:22 -0400

    added more table dictionaries; added separate parsing function for lob_lobbyist table to handle special cases

--- master.py
+++ master.py
@@ -2,14 +2,42 @@ import json
 from os.path import join
 import pandas as pd
 
-# Read in files 
+# Read in file data
 with open('data_dictionary.json') as f:
     data_dictionary = json.load(f)
+    categories = list(data_dictionary.keys())
 
-categories = list(data_dictionary.keys())
+
+# lob_lobbyist data is a special case
+# some records are split across multiple lines
+def parse_lob_lobbyist(path, columns):
+    parsed_data = []
+    bad_data = []
+    with open(path, 'rb') as f:
+        lines = f.read().splitlines()
+        for i, line in enumerate(lines):
+            line_data = line.decode(errors='ignore').split('|')
+            line_parsed = line_data[1::2]
+            if len(line_parsed) != 8:
+                bad_data.append(line.decode(errors='ignore'))
+                continue
+            else:            
+                parsed_data.append(line_parsed)
+    
+    temp_line = ""
+    parsed_bad_data = []
+    for line in bad_data:
+        temp_line += line
+        if len(temp_line.split('|')[1::2]) == 8:
+            parsed_bad_data.append(temp_line.split('|')[1::2])
+            temp_line = ""
+
+    df = pd.DataFrame(parsed_data + parsed_bad_data, columns=columns)
+
+    return df
 
 
-def get_col_info(fields):
+def get_df_from_txt_file(path, fields):
     types_parse = {
         'varchar': 'string',
         'text': 'string',
@@ -18,35 +46,43 @@ def get_col_info(fields):
         'number': 'Float64',
         'int': 'Int32',
         'integer': 'Int32',
-        'datetime': 'string'
+        'datetime': 'string',
+        'date': 'string'
     }
-    dtypes = { field['field'] : types_parse[field['type'].lower()] for field in fields}
-    col_names = [field['field'] for field in fields]
 
-    return (col_names, dtypes)
+    dtypes = { field['field'] : types_parse[field['type'].lower()] for field in fields }
+    col_names = [field['field'] for field in fields]
+    
+    parse_dates = [field['field'] for field in fields if field['type'].lower().contains('date')]
+
+    if 'lob_lobbyist' in path:
+        df = parse_lob_lobbyist(path, col_names)
+    else:
+        df = pd.read_csv(path,
+                engine='python',
+                header=None,
+                dtype=dtypes,
+                parse_dates=parse_dates,
+                names=col_names,
+                quotechar='|',
+                encoding_errors='ignore'
+        )
+
+    return df
 
 
 for category, cat_dict in data_dictionary.items():
-    if category == "Lobby":
-        continue
     print("Reading category %s..." % category)
     tables = list(cat_dict['tables'].keys())
     for table in tables:
         print("Reading table %s..." % table)
         path = join('data/', category, '%s.txt' % table)
         params = cat_dict['tables'][table]
-        # df = get_df_from_txt(path, params)
         total_records = params['record_count']
-        col_names, dtypes = get_col_info(params['fields'])
-        parse_dates = [col for col, dtype in dtypes.items() if dtype == 'datetime64']
-        df = pd.read_csv(path,
-                header=None,
-                dtype=dtypes,
-                parse_dates=parse_dates,
-                names=col_names,
-                quotechar='|',
-                encoding_errors='ignore')
+
+        df = get_df_from_txt_file(path, params['fields'])
         
-        assert df.shape[0] == total_records
+        if df.shape[0] != total_records:
+            print(f"ERROR: {path} has {df.shape[0]} records, but should have {total_records}")
 
         print("Finished reading %d rows" % df.shape[0])
